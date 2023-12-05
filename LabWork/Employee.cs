869da52b3c8b786865081bc5a19173038e407272
nutzern4me <z4kh4rz@yandex.ru>
2023-12-05 19:00:08 +0400

    lab6

---
+++ LabWork/Employee.cs
@@ -0,0 +1,36 @@
+﻿namespace LabWork.Employees;
+
+/// <summary>
+/// Кадр (сотрудник)
+/// </summary>
+internal class Employee
+{
+    public string Name { get; set; } = null!;
+
+    public virtual string EmployeeType => "Сотрудник";
+
+    public Employee(string name)
+    {
+        Name = name;
+    }
+
+    public void DoWork()
+    {
+        Console.WriteLine($"{this.ToString} делает свою работу");
+    }
+
+    public override string ToString()
+    {
+        return $"{EmployeeType} {Name}";
+    }
+
+    public override bool Equals(object? obj)
+    {
+        if (this == obj) 
+            return true;
+        else if (obj is Employee employee && employee.Name.Equals(this.Name, StringComparison.OrdinalIgnoreCase))
+            return true;
+        
+        return false;
+    }
+}
--- LabWork/Program.cs
+++ LabWork/Program.cs
@@ -1,23 +1,50 @@
-﻿namespace LabWork;
+﻿using LabWork.Employees;
+
+namespace LabWork;
 
 internal class Program
 {
     static void Main(string[] args)
     {
-        var set1 = new Cset<int>(1, 2, 3, 4, 4, 4, 5, 5, 5);
-        set1.PrintAll(nameof(set1));
-        Console.WriteLine();
+        var doubleSet1 = new Cset<double>(1, 2, 3, 4, 4, 4, 5, 5, 5);
+        var doubleSet2 = new Cset<double>(3, 3, 3, 4, 5, 6, 7);
+        TestSets(doubleSet1, doubleSet2, 8);
 
-        var set2 = new Cset<int>(3, 3, 3, 4, 5, 6, 7);
+        var charSet1 = new Cset<char>('a', 'b', 'c', 'd', 'e');
+        var charSet2 = new Cset<char>('d', 'e', 'f', 'g', 'h');
+        TestSets(charSet1, charSet2, 'z');
+
+        var employeeSet1 = new Cset<Employee>(
+            new Employee("Иван Петров"),
+            new Employee("Владимир Сергеев"),
+            new Employee("Антон Никитин")
+        );
+        var employeeSet2 = new Cset<Employee>(
+            new Employee("Дмитрий Лазарев"),
+            new Employee("Владимир Сергеев"),
+            new Employee("Николай Алексеев")
+        );
+        TestSets(employeeSet1, employeeSet2, new Employee("Денис Давыдов"));
+
+        
+        Console.WriteLine("\nУдаление каждого второго элемента в множестве " + nameof(doubleSet1));
+        DeleteEvenElemsTest(doubleSet1);
+
+        Console.WriteLine("\nУдаление каждого второго элемента в множестве " + nameof(charSet1));
+        DeleteEvenElemsTest(charSet1);
+
+        Console.WriteLine("\nУдаление каждого второго элемента в множестве " + nameof(employeeSet1));
+        DeleteEvenElemsTest(employeeSet1);
+    }
+
+    private static void TestSets<T>(Cset<T> set1, Cset<T> set2, T itemToAdd)
+    {
+        set1.PrintAll(nameof(set1));
         set2.PrintAll(nameof(set2));
         Console.WriteLine();
 
-        var set3 = new Cset<int>(7, 6, 5, 4, 3);
-        set3.PrintAll(nameof(set3));
-        Console.WriteLine();
-
-        Console.WriteLine("Добавление элемента 8 в множество " + nameof(set1));
-        set1 += 8;
+        Console.WriteLine($"Добавление элемента {itemToAdd} в множество");
+        set1 += itemToAdd;
         set1.PrintAll(nameof(set1));
         Console.WriteLine();
 
@@ -32,10 +59,37 @@ internal class Program
         Console.WriteLine();
 
         Console.WriteLine($"Проверка множеств {nameof(set1)} и {nameof(set2)} на равенство: {set1 == set2}");
-        Console.WriteLine($"Проверка множеств {nameof(set2)} и {nameof(set3)} на равенство: {set2 == set3}");
-        
+
         Console.WriteLine($"Мощность множества {nameof(set1)}: {(int)set1}");
         Console.WriteLine($"Мощность множества {nameof(set2)}: {(int)set2}");
-        Console.WriteLine($"Мощность множества {nameof(set3)}: {(int)set3}");
+        Console.WriteLine();
+    }
+
+    private static void DeleteEvenElemsTest<T>(Cset<T> set)
+    {
+        set.PrintAll("до удаления");
+
+        DeleteSetEvenElements(ref set);
+        
+        set.PrintAll("после удаления");
+    }
+
+    /// <summary>
+    /// Удаление каждого второго элемента в множестве
+    /// </summary>
+    private static void DeleteSetEvenElements<T>(ref Cset<T> set)
+    {
+        var resultSet = new Cset<T>();
+
+        int i = 1;
+        foreach (var item in set)
+        {
+            if (i % 2 != 0)
+                resultSet.Add(item);
+            
+            i++;
+        }
+
+        set = resultSet;
     }
 }
